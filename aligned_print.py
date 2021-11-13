ab4a1bb7fbf9bfc44eeb6929bd223283b31434ee
Nitul Datt <nitul1991@gmail.com>
2021-11-14 00:56:38 +0530

    Initial commit

---
+++ aligned_print.py
@@ -0,0 +1,23 @@
+
+
+import contextlib
+import logging
+
+
+@contextlib.contextmanager
+def printer(print_function=print):
+    """A context manager that prints key value pairs, with the keys being
+    printed with a fixed width that is equal to the width of the longest key
+    """
+    keys   = []
+    values = []
+    def printer_impl(key, value):
+        keys.append(key)
+        values.append(value)
+
+    try:
+        yield printer_impl
+    finally:
+        max_key_width = max(map(len, keys))
+        for key, value in zip(keys, values):
+            print_function(f'{key:{max_key_width}} => {value}')
---
+++ test_aligned_print.py
@@ -0,0 +1,46 @@
+
+
+import aligned_print
+import io
+
+
+def test_basic():
+    output = io.StringIO()
+    print_function = lambda msg: output.write(f'{msg}\n')
+    with aligned_print.printer(print_function) as print_:
+        print_('a', 'a')
+        print_('bb', 'b')
+        print_('ccc', 'c')
+        print_('dddd', 'd')
+        print_('eeeee', 'e')
+
+    expected = """
+a     => a
+bb    => b
+ccc   => c
+dddd  => d
+eeeee => e
+"""
+    assert(output.getvalue().strip('\n') == expected.strip('\n'))
+
+
+def test_attribute_printing():
+    output = io.StringIO()
+    print_function = lambda msg: output.write(f'{msg}\n')
+
+    class Foo(object):
+        def __init__(self):
+            self.name = 'foo'
+            self.id   = 10
+            self.age  = 29
+
+    with aligned_print.printer(print_function) as print_:
+        foo = Foo()
+        list(map(lambda pair: print_(pair[0], pair[1]), foo.__dict__.items()))
+
+    expected = """
+name => foo
+id   => 10
+age  => 29
+"""
+    assert(output.getvalue().strip('\n') == expected.strip('\n'))
